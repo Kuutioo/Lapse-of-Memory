a0633d04728ba573a98e32923a6c253f3a1836cd
Kuutioo <ape.hakkarainen@gmail.com>
2022-04-21 12:55:53 +0300

    Player can pickup objects

---
+++ Lapse of Memory/Assets/Scripts/Player/GrabObject.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabObject : MonoBehaviour
+{
+    private GameObject grabbedObject;
+
+    [SerializeField] private float pickupRange;
+    [SerializeField] private float moveForce = 250;
+    [SerializeField] private Transform holdParent;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (grabbedObject == null) 
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, pickupRange))
+                {
+                    PickupObject(hit.transform.gameObject);
+                }
+            }
+            else
+            {
+                DropObject();
+            }
+        }
+
+        if(grabbedObject != null)
+        {
+            MoveObject();
+        }
+    }
+
+    private void MoveObject()
+    {
+        if(Vector3.Distance(grabbedObject.transform.position, holdParent.position) > 0.1f)
+        {
+            Vector3 moveDirection = (holdParent.position - grabbedObject.transform.position);
+            grabbedObject.GetComponent<Rigidbody>().AddForce(moveDirection * moveForce);
+        }
+    }
+
+    private void PickupObject(GameObject pickObject)
+    {
+        if (pickObject.GetComponent<Rigidbody>())
+        {
+            Rigidbody objectRigidbody = pickObject.GetComponent<Rigidbody>();
+            objectRigidbody.useGravity = false;
+            objectRigidbody.drag = 10;
+
+            objectRigidbody.transform.parent = holdParent;
+            grabbedObject = pickObject;
+        }
+    }
+
+    private void DropObject()
+    {
+        Rigidbody heldRigidbody = grabbedObject.GetComponent<Rigidbody>();
+        heldRigidbody.useGravity = true;
+        heldRigidbody.drag = 1;
+
+        grabbedObject.transform.parent = null;
+        grabbedObject = null;
+    }
+}
